22d548dddb55149f7933ba8f9c3d550ed4bc5ca6
adarsh singh <adsredmi182615@gmail.com>
2025-04-29 19:11:33 +0530

    fixed user's frontend

--- apps/webhook/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-app.post('/',(req,res)=>{
-    const paymentInformation ={
-        token:req.body.token,
-        userId:req.body.userId_identifier,
-        amount:req.body.amount,
-    }
-})
